fce7a4853b5683cf1823afb8104c0b1f8cb5c466
daniel-j-dev <danielaugustusjohnson@gmail.com>
2020-12-09 19:41:21 -0500

    Added code block w/ scroll bar and new font

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -1,11 +1,33 @@
-import './CodeBlock.css';
+import { useContext } from "react"
+import appContext from "../../contexts/appContext"
+import "./CodeBlock.css"
 
 function CodeBlock() {
+  const { settings } = useContext(appContext)
+  const algo = settings.selectedAlgo
+
+  const bubbleSort = `const bubbleSort = array => {
+    let swapped = true
+    while (swapped) {
+      swapped = false
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[i + 1]) {
+          ;[array[i], array[i + 1]] = [array[i + 1], array[i]]
+          swapped = true
+        }
+      }
+    }
+    return array
+  }`
+
   return (
-    <div id='CodeBlock'>
-      
+    <div id="code-container">
+      <div id="code-label">
+        <span>Source Code</span>
+      </div>
+      <code>{algo.name === "bubbleSort" ? bubbleSort : ""}</code>
     </div>
-  );
+  )
 }
 
-export default CodeBlock;
+export default CodeBlock
